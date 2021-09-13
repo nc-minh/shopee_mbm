eef927c7addb3b280dc77edcbfc33e1080efc45d
nguyencongminh-dev <nguyencongminh.official@gmail.com>
2021-09-13 18:33:51 +0700

    authod: ncm, update: slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,25 +47,13 @@ new Splide( '.splide-3', {
 } ).mount();
 
 
-const slider = document.querySelector('.body__banner__wrapper--left--slide')
 const list = document.querySelector('.body__banner__wrapper--left__wrapper')
 const sliderItems = document.querySelectorAll('.body__banner__wrapper--left__wrapper--item')
-const prevBtn = document.querySelector('.body__banner__wrapper--left--prev')
-const nextBtn = document.querySelector('.body__banner__wrapper--left--next')
 const dotItem = document.querySelectorAll('.body__banner__wrapper--left--slide__dot-item')
-
-// var itemWidth = sliderItems[0].offsetWidth
 var itemWidth = 100
 const sliderItemLength = sliderItems.length
 let positionX = 0
 let index = 0
-
-// itemWidth = ((itemWidth * sliderItemLength) / 100)
-
-// console.log(sliderItemLength);
-// console.log(itemWidth);
-// console.log(positionX);
-
 
 
 
@@ -75,17 +63,11 @@ function sliderShowPrev(){
         list.style = `transform: translateX(-${(itemWidth*sliderItemLength) - itemWidth}%)`
         index = sliderItemLength - 1
         positionX = ((itemWidth*sliderItemLength) - itemWidth)
-        // console.log('trong if');
     }else{
-        // console.log('index', index);
         positionX = (positionX - itemWidth)
         list.style = `transform: translateX(-${positionX}%)`
-        // console.log(positionX);
-        // console.log('next');
-        // console.log('trong else');
     }
 }
-
 function sliderShowNext(){
     index++
     // console.log(index);
@@ -95,13 +77,8 @@ function sliderShowNext(){
         positionX = 0
     }else{
         positionX = positionX + itemWidth
-        // console.log(itemWidth);
         list.style = `transform: translateX(-${positionX}%)`
-        // console.log(positionX);
-        // console.log('next');
     }
-    
-    
 }
 
 function handleSlider(n){
@@ -110,8 +87,6 @@ function handleSlider(n){
     }else if(n == -1){
         sliderShowNext()
     }
-
-    // console.log('check index', index);
     for (let i = 0; i < dotItem.length; i++) {
         dotItem[i].style.backgroundColor = 'white'
         dotItem[index].style.backgroundColor = 'red'
@@ -124,6 +99,10 @@ setInterval(()=>{
 
 function dotNext(n){
     list.style = `transform: translateX(-${n*100}%)`
+    for (let i = 0; i < dotItem.length; i++) {
+        dotItem[i].style.backgroundColor = 'white'
+        dotItem[n].style.backgroundColor = 'red'
+    }
 }
 
 
